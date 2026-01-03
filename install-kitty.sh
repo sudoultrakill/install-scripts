d0645b95f3cf90d4061d1c2234067864feb7d293
sudoultrakill <sudoultrakill@protonmail.com>
2026-01-02 22:55:29 -0500

    Update install-kitty.sh

--- install-kitty.sh
+++ install-kitty.sh
@@ -1,1 +1,21 @@
+sudo pacman -Syu kitty zsh
+
+echo "Creating kitty config ..."
+mkdir ~/.config/kitty
+touch ~/.config/kitty.conf
+
+echo "Changing default shell to zsh..."
+sudo chsh -s $(which zsh)
+
+echo "Installing oh-my-zsh plugin manager for zsh ..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+
+echo "Installing zsh-syntax-highlighting ..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "Installing powerlevel10k..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+echo "Sourcing ~/.zshrc ..."
+source ~/.zshrc
 
